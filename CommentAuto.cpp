d7a6cac208325ac6ef850bb72bcc975e28aaeea4
Trevor <trevormheath@gmail.com>
2021-09-16 21:59:42 -0600

    Updates

--- CommentAuto.cpp
+++ CommentAuto.cpp
@@ -28,37 +28,44 @@ void CommentAuto::S1(const std::string& input) {
 }
 
 void CommentAuto::S2(const std::string& input) {
-    if (input[index] == '|') {
-        inputRead++;
-        index++;
-        S3(input);
-    }
-    else {
-        if (input[index] == '\n'){
-            newLines++;
+    if (!input.empty()) {
+        if (input[0] == '|') {
+            inputRead++;
+            index++;
+            S3(input.substr(1));
+        } else {
+            if (input[0] == '\n') {
+                newLines++;
+            }
+            inputRead++;
+            index++;
+            S2(input.substr(1));
         }
-        inputRead++;
-        index++;
-        S2(input);
+    }
+    else{
+        Serr();
     }
 }
 
 void CommentAuto::S3(const std::string& input) {
-    if (input[index] == '#') {
-        inputRead++;
-        index++;
-    }
-    else if (input[index] == '|'){
-        inputRead++;
-        index++;
-        S3(input);
-    }
-    else {
-        if (input[index] == '\n'){
-            newLines++;
+    if (!input.empty()) {
+        if (input[0] == '#') {
+            inputRead++;
+            index++;
+        } else if (input[0] == '|') {
+            inputRead++;
+            index++;
+            S3(input.substr(1));
+        } else {
+            if (input[0] == '\n') {
+                newLines++;
+            }
+            inputRead++;
+            index++;
+            S2(input.substr(1));
         }
-        inputRead++;
-        index++;
-        S2(input);
+    }
+    else{
+        Serr();
     }
 }
